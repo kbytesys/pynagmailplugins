b13c36483f018e024886e6974ed577add2fb81e7
kbyte <kbytesys@gmail.com>
2015-07-23 13:58:32 +0200

    namespace_packages

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pynagmailplugins',
     packages=find_packages(exclude=['docs', 'samples', 'tests']),
+    namespace_packages=['snowpenguin'],
     version='0.0.1',
     scripts=['bin/check_mailq.py'],
     install_requires=[
